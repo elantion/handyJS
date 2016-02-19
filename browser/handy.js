f7b19835071c843f2ad17782ffd456108cebdc6b
JamesYin <elantion@sina.com>
2016-02-19 13:45:55 +0800

    forgot changing variable name

--- browser/handy.js
+++ browser/handy.js
@@ -2,16 +2,16 @@
 
 var handyJS = {};
 //All bout file manipulate
-util.file = {};
+handyJS.file = {};
 
 // sources:
 // http://stackoverflow.com/questions/190852/how-can-i-get-file-extensions-with-javascript
-util.file.getFileExtension = function (fileName) {
+handyJS.file.getFileExtension = function (fileName) {
 	return fileName.slice((Math.max(0, fileName.lastIndexOf('.')) || Infinity) + 1);
 };
 
 //usage: changeFileName('ding.js', 'dong'); => dong.js
-util.file.changeFileName = function (originalName, newName) {
+handyJS.file.changeFileName = function (originalName, newName) {
 	var extension = this.getFileExtension(originalName);
 	return newName + '.' + extension;
 };
