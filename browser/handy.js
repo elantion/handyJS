88dbc976b3ec7fca7d6b9af0386fd86a7c0f4284
JamesYin <elantion@sina.com>
2016-02-19 13:54:43 +0800

    change function name

--- browser/handy.js
+++ browser/handy.js
@@ -6,12 +6,12 @@ handyJS.file = {};
 
 // sources:
 // http://stackoverflow.com/questions/190852/how-can-i-get-file-extensions-with-javascript
-handyJS.file.getFileExtension = function (fileName) {
+handyJS.file.getExtension = function (fileName) {
 	return fileName.slice((Math.max(0, fileName.lastIndexOf('.')) || Infinity) + 1);
 };
 
 //usage: changeFileName('ding.js', 'dong'); => dong.js
-handyJS.file.changeFileName = function (originalName, newName) {
+handyJS.file.changeName = function (originalName, newName) {
 	var extension = this.getFileExtension(originalName);
 	return newName + '.' + extension;
 };
